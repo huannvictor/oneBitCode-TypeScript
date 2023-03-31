327cbb5396380c7926961493146e6825acedd929
Huann Almeida <huannvictor@gmail.com>
2023-03-31 17:42:15 -0300

    .

--- index.ts
+++ index.ts
@@ -1,21 +1,37 @@
-const setPilot = (newPilot: string, spaceship: { name?: string; pilot: string; speed?: number; inMission?: boolean; }) => {
+const setPilot = async (
+  newPilot: string,
+  spaceship: {
+    name?: string;
+    pilot: string;
+    speed?: number;
+    inMission?: boolean;
+  }
+) => {
   spaceship.pilot = newPilot;
-}
+};
 
-const accelerate = (targetSpeed: number, spaceship: { name?: string; pilot?: string; speed: any; inMission?: boolean; }) => {
+const accelerate = async (
+  targetSpeed: number,
+  spaceship: { name?: string; pilot?: string; speed: any; inMission?: boolean }
+) => {
   spaceship.speed = targetSpeed;
-}
+};
 
-const sendToMission = (spaceship: { name?: string; pilot?: string; speed?: number; inMission: any; }) => {
+const sendToMission = async (spaceship: {
+  name?: string;
+  pilot?: string;
+  speed?: number;
+  inMission: any;
+}) => {
   spaceship.inMission = true;
-}
+};
 
 const spaceship = {
-  name: '',
-  pilot: '',
+  name: "",
+  pilot: "",
   speed: 0,
   inMission: false,
-}
+};
 
 const pilot = "Han Solo";
 
